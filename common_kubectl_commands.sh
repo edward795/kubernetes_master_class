ce590ebb011f55c2a634c96722ccbe8be1b13743
ayyappadas-gb <ayyappadas.gb@ibm.com>
2025-11-30 16:30:12 +0530

    basics of kubernetes

---
+++ common_kubectl_commands.sh
@@ -0,0 +1,11 @@
+kubectl get nodes
+kubectl get pods 
+kubectl get pods -A
+kubectl get pods -o wide 
+kubectl describe pod <pod_name>
+kubectl logs <pod_name>
+kubectl exec -it <pod_name> -- bash
+kubectl run <podname> --image=<image_name>
+kubectl --kubeconfig <config_file_name>
+kubectl apply -f <manifest.yaml/.json>
+kubectl delete -f <manifest.yaml/.json>
