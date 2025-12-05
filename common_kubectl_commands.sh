e801cf9002f7b377c5f71f3501b05fe22194f4d2
ayyappadas-gb <ayyappadas.gb@ibm.com>
2025-12-05 13:26:19 +0530

    command line args in kubectl

--- common_kubectl_commands.sh
+++ common_kubectl_commands.sh
@@ -9,3 +9,6 @@ kubectl run <podname> --image=<image_name>
 kubectl --kubeconfig <config_file_name>
 kubectl apply -f <manifest.yaml/.json>
 kubectl delete -f <manifest.yaml/.json>
+kubectl api-resources
+kubectl run <podname> --image=<image_name> --dry-run=client -o yaml
+kubectl exec -it <multi-container-pod-name> -c <exec_into_container> -- bash
